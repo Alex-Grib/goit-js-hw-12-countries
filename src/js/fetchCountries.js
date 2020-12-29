41a8e79dbda0f789c771ee65a37f960d9ba17d7f
Alexandr Grib <alexgribaldi@gmail.com>
2020-12-29 23:17:20 +0100

    убирает реакцию на ошибку

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,25 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 
-export default function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`).then(response => {
-    if (response.status === 404) return;
-    return response.json();
-  });
-}
+export default function fetchCountries(searchQuery){
+if (searchQuery) {
+  return fetch(
+    `${BASE_URL}/${searchQuery}`,
+  )
+    .then(response => {
+      if (response.ok) return response.json()
+      throw new Error(`Error feching data`);
+    })
+  
+    .catch(error => {
+      console.error(`Error:`, error);
+    });
+  }
+  }
+
+
+// export default function fetchCountries(searchQuery) {
+//   return fetch(`${BASE_URL}/${searchQuery}`).then(response => {
+//     if (response.status === 404) return;
+//     return response.json();
+//   });
+// }
